5894c5d6d2680833d888590e29ee452faa813b4d
oyuyump <yuyumpoptec@gmail.com>
2024-10-26 04:43:00 -0600

    Add a new receipt

--- Barman/Controler/AddRecipeViewController.swift
+++ Barman/Controler/AddRecipeViewController.swift
@@ -9,13 +9,44 @@ import UIKit
 
 class AddRecipeViewController: UIViewController {
 
+    @IBOutlet weak var txtDrinkName: UITextField!
+    
+    @IBOutlet weak var txtIngredients: UITextField!
+    
+    @IBOutlet weak var txtInstructions: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnAdd(_ sender: Any) {
+        var msg: String = ""
+        
+        guard let name = txtDrinkName.text,
+              let ingredients = txtIngredients.text,
+              let instructions = txtInstructions.text
+        else { return }
+        
+        if name.isEmpty {
+            msg += "Please enter a name for the recipe.\n"
+        }
+        if ingredients.isEmpty {
+            msg += "Please enter ingredients for the recipe.\n"
+        }
+        if instructions.isEmpty {
+            msg += "Please enter instructions for the recipe.\n"
+        }
+        
+        
+        if msg.isEmpty {
+            let newRecipe: [[String: Any]] = [["name": name, "ingredients": ingredients, "directions": instructions, "img": ""]]
+            
+            DataManager.shared.saveRecipes(newRecipe)
+            navigationController?.popViewController(animated: true)
+        }else{
+            print(msg)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
